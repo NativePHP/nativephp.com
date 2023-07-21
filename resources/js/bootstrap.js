3c30a52538a4ce7bbc96c40111ebddd98ee17083
Jan Willem Kolkman <jwkolkman@gmail.com>
2023-07-21 10:25:44 +0200

    Import Alpine.js into the bootstrap file. This fixes issue #5

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,12 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Import Alpine.js
+ */
+import Alpine from 'alpinejs'
+Alpine.start()
+
+/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
