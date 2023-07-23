d1b5445cc5d4bfd0e6ad216998ab3059adc79515
= <=>
2023-07-22 21:55:48 -0700

    Fix navigation toggling issue on mobile devices

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,16 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/docs/navigation.blade.php
+++ resources/views/docs/navigation.blade.php
@@ -1,5 +1,4 @@
-<div
-    class="hidden lg:block absolute z-20 w-[18rem] inset-0 top-24 left-[max(0px,calc(50%-45rem))] right-auto ">
+<div class="hidden lg:block absolute z-20 w-[18rem] inset-0 top-24 left-[max(0px,calc(50%-45rem))] right-auto ">
 
     <div class="w-full border-r border-red-600 border-opacity-10 pt-8 pb-12 pl-4">
 
@@ -12,29 +11,22 @@
 
 <div class="lg:hidden my-8" x-data="{ showDocsNavigation: false }">
     <div class="flex justify-end">
-        <button type="button"
-                class="p-4"
-                @click="showDocsNavigation = !showDocsNavigation"
-        >
+        <button type="button" class="p-4" @click="showDocsNavigation = !showDocsNavigation">
             <div x-show="!showDocsNavigation">
-                <x-icons.menu class="text-red-600 w-6 h-6"/>
+                <x-icons.menu class="text-red-600 w-6 h-6" />
             </div>
             <div x-show="showDocsNavigation">
-                <x-icons.close class="w-6 h-6"/>
+                <x-icons.close class="w-6 h-6" />
             </div>
         </button>
     </div>
 
-    <nav x-show="showDocsNavigation"
-         x-transition:enter="transition ease-out duration-200"
-         x-transition:enter-start="translate-y-1 opacity-0"
-         x-transition:enter-end="translate-y-0 opacity-100"
-         x-transition:leave="transition ease-in duration-150"
-         x-transition:leave-start="translate-y-0 opacity-100"
-         x-transition:leave-end="translate-y-1 opacity-0"
-         class="mt-2 px-4 pt-4 border-b border-red-600 border-opacity-50"
-    >
-        {!!  $navigation !!}
+    <nav x-show="showDocsNavigation" x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="translate-y-1 opacity-0" x-transition:enter-end="translate-y-0 opacity-100"
+        x-transition:leave="transition ease-in duration-150" x-transition:leave-start="translate-y-0 opacity-100"
+        x-transition:leave-end="translate-y-1 opacity-0"
+        class="mt-2 px-4 pt-4 border-b border-red-600 border-opacity-50">
+        {!! $navigation !!}
     </nav>
 
 </div>
