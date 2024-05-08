e8f2c1888c591ac251ff88b53ca55549b7c6f1d3
Simon Hamp <simon.hamp@me.com>
2024-05-08 12:32:31 +0100

    Add text-balance

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-center bg-teal-500 p-2 text-center text-sm text-white">
+<div class="flex items-center justify-center bg-teal-500 p-2 text-center text-sm text-white text-balance">
     <a href="https://github.com/orgs/NativePHP/discussions/278" onclick="fathom.trackEvent('alert_click');">
         Windows support is here! 🔥 Read the full announcement &rightarrow;
     </a>
