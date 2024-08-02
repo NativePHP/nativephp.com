40987ce3735e5083cc05fa85f3615915201c3526
Simon Hamp <simon.hamp@me.com>
2024-08-02 16:33:05 +0100

    Improve alpha messaging

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -7,13 +7,16 @@
             <div class="lg:pt-4">
                 <div class="max-w-3xl mx-auto xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
 
-                    <div class="flex items-center px-6 py-4 mb-8 space-x-4 text-lg text-red-800 border-2 border-red-500 bg-red-50 dark:text-white dark:bg-red-800">
-                        <span class="text-5xl">⚠️</span>
+                    <div class="flex px-6 py-4 mb-8 space-x-4 text-lg text-orange-800 border-2 border-orange-500 bg-orange-50 dark:text-white dark:bg-orange-800">
+                        <x-heroicon-o-shield-exclamation class="size-10" />
                         <span>
-                            NativePHP is currently in <em class="font-bold">alpha</em>. We do not recommend distributing
-                            production releases of your applications yet.<br><br>
-                            <a href="https://github.com/sponsors/simonhamp" class="underline" target="_blank">
-                                Can you help us get production-ready?
+                            NativePHP is currently
+                            <a href="/docs/getting-started/status" class="font-bold italic font-mono flex-inline px-2 text-base bg-orange-200 rounded">alpha</a>.
+                            <br><br>
+                            <a href="https://github.com/nativephp/laravel?sponsor=1" class="font-bold inline-flex items-center bg-orange-200 rounded-md px-3 py-1 border border-orange-400 hover:bg-orange-300 group" target="_blank">
+                                Let's get to beta!
+                                <x-heroicon-o-rocket-launch class="ml-2 size-5 group-hover:hidden" />
+                                <x-heroicon-s-rocket-launch class="hidden ml-2 size-5 group-hover:block" />
                             </a>
                         </span>
                     </div>
