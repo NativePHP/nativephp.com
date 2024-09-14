e0b0201b7fdd2ff9f44cecedb9debad5d5ec06d3
Eser DENIZ <srwiez@gmail.com>
2024-09-14 15:50:46 +0200

    pinkary.blade.php && accessibility

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-<div class="lg:sticky top-0 relative text-white bg-gray-100 z-50 dark:bg-gray-800">
+<div class="lg:sticky top-0 relative text-white bg-gray-50 border-b border-gray-100 z-50 dark:bg-gray-800">
     <div
         class="flex items-center justify-between gap-6 px-6 py-3 mx-auto max-w-7xl sm:py-4"
     >
@@ -14,15 +14,15 @@
         <div class="flex items-center space-x-6">
             <div id="docsearch" class="mr-12"></div>
 
-            <a href="https://discord.gg/X62tWNStZK">
+            <a href="https://discord.gg/X62tWNStZK" title="Go to discord server">
                 <x-icons.discord class="size-5 dark:fill-white" />
             </a>
 
-{{--            <a href="https://pinkary.com/@nativephp">--}}
-{{--                <x-icons.discord class="size-5 dark:fill-white" />--}}
-{{--            </a>--}}
+            <a href="https://pinkary.com/@nativephp" title="Pinkary of NativePHP">
+                <x-icons.pinkary class="size-5 dark:fill-white" />
+            </a>
 
-            <a href="https://github.com/nativephp">
+            <a href="https://github.com/nativephp" title="Source code of NativePHP">
                 <x-icons.github class="size-5 dark:fill-white" />
             </a>
         </div>
