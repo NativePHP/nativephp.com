9c7baddf2352e05df5086e9a2736337955344da9
Eser DENIZ <srwiez@gmail.com>
2024-09-15 15:26:20 +0200

    Linkedin + open collective

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -10,16 +10,24 @@
             <img src="{{ asset('logo-dark.svg') }}" class="hidden h-8 dark:block">
             <span class="sr-only">NativePHP</span>
         </a>
+        <div id="docsearch"></div>
 
-        <div class="flex items-center space-x-6">
-            <div id="docsearch" class="mr-12"></div>
+        <div class="hidden md:flex items-center space-x-5">
+
+            <a href="https://www.linkedin.com/company/nativephp/" title="NativePHP on LinkedIn">
+                <x-icons.linkedin class="size-5 text-black dark:text-white hover:text-[#00aaa6]" />
+            </a>
 
             <a href="https://discord.gg/X62tWNStZK" title="Go to discord server">
                 <x-icons.discord  class="size-5 dark:fill-white hover:fill-[#00aaa6]" />
             </a>
 
-            <a href="https://pinkary.com/@nativephp" title="Pinkary of NativePHP">
+            <a href="https://pinkary.com/@nativephp" title="NativePHP on Pinkary">
                 <x-icons.pinkary class="size-5 text-black dark:invert hover:text-[#00aaa6] hover:invert-0" />
+            </a>
+
+            <a href="https://opencollective.com/nativephp" title="NativePHP on LinkedIn">
+                <x-icons.opencollective class="size-5 text-black dark:invert hover:text-[#00aaa6] hover:invert-0" />
             </a>
 
             <a href="https://github.com/nativephp" title="Source code of NativePHP">
