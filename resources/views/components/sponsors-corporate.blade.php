17b838a019f19507e0e2137c113719fc701297de
Simon Hamp <simon.hamp@me.com>
2025-01-11 22:59:35 +0000

    Sponsor

--- resources/views/components/sponsors-corporate.blade.php
+++ resources/views/components/sponsors-corporate.blade.php
@@ -1,10 +1,10 @@
 @props(['height' => 'h-12'])
 
-<a href="https://www.redgalaxy.co.uk/" title="RedGalaxy sponsor">
+<a href="https://www.redgalaxy.co.uk/" title="RedGalaxy">
     <x-sponsors.redgalaxy class="block {{ $height }} max-w-full"/>
 </a>
 
-<a href="https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage" title="Sevalla sponsor">
+<a href="https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage" title="Sevalla">
     <x-sponsors.sevalla class="{{ $height }} text-black dark:text-white max-w-full"/>
 </a>
 
@@ -14,6 +14,10 @@
 </a>
 --}}
 
-<a href="https://www.kaashosting.nl/?lang=en" class="block {{ $height }}" title="KaasHosting sponsor">
+<a href="https://www.kaashosting.nl/?lang=en" class="block {{ $height }}" title="KaasHosting">
     <x-sponsors.kaashosting class="block {{ $height }} fill-[#042340] dark:fill-white max-w-full"/>
+</a>
+
+<a href="https://borah.digital/" class="block {{ $height }}" title="Borah Digital Labs">
+    <x-sponsors.borah class="block {{ $height }} fill-[#042340] dark:fill-white max-w-full"/>
 </a>
---
+++ resources/views/components/sponsors/borah.blade.php
@@ -0,0 +1,1 @@
+<img src="/img/sponsors/borah.png" class="block h-14 w-auto max-w-full">
