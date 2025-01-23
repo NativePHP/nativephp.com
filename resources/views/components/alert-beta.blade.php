4e66d1bcf8dcdfed8e97d53a01e43ade584bf27d
Simon Hamp <simon.hamp@me.com>
2025-01-23 00:19:39 +0000

    tidy

--- resources/views/components/alert-beta.blade.php
+++ resources/views/components/alert-beta.blade.php
@@ -14,11 +14,10 @@
         </p>
 
         <a href="/docs/getting-started/status"
-           onclick="fathom.trackEvent('beta_interest');"
-           class="group mt-4 font-bold inline-flex items-center rounded-md px-3 py-1
-                            bg-orange-200 hover:bg-orange-300
-                            dark:bg-orange-900 dark:hover:bg-orange-500 dark:text-orange-100
-                            " target="_blank">
+            onclick="fathom.trackEvent('beta_interest');"
+            class="group mt-4 font-bold inline-flex items-center rounded-md px-3 py-1
+                bg-orange-200 hover:bg-orange-300
+                dark:bg-orange-900 dark:hover:bg-orange-500 dark:text-orange-100">
             Let's get to v1!
             <x-heroicon-o-rocket-launch class="ml-2 size-5 group-hover:hidden"/>
             <x-heroicon-s-rocket-launch class="hidden ml-2 size-5 group-hover:block"/>
