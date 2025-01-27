842f62624a1fd593fd64cb6e779c8b6b89796980
Eser DENIZ <srwiez@gmail.com>
2025-01-27 14:26:41 +0100

    fix: retrofit laravel-vite changes (#77)
    * fix: retrofit laravel-vite changes
    
    * fix: remove unused import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    server: {
+        cors: {
+            origin: [
+                /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/,
+                /^https?:\/\/.*\.test(:\d+)?$/,          // Valet / Herd    (SCHEME://*.test:PORT)
+            ],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
