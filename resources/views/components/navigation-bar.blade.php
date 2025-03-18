22a59d6d2ae377e44142536f7c17498bd37bfd53
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-18 22:50:14 +0330

    ✨ Improve navigation bar responsiveness and styling; adjust logo size and enhance search button visibility

--- resources/views/components/navigation-bar.blade.php
+++ resources/views/components/navigation-bar.blade.php
@@ -14,19 +14,19 @@
         {{-- Logo --}}
         <div class="flex items-center gap-3">
             <a href="/">
-                <x-logo class="h-6" />
+                <x-logo class="h-5 sm:h-6" />
                 <span class="sr-only">NativePHP</span>
             </a>
             <div
-                class="rounded-full bg-gray-200/60 px-2 py-1 text-xs text-gray-600"
+                class="hidden rounded-full bg-gray-200/60 px-2 py-1 text-xs text-gray-600 lg:block"
             >
                 1.0.0-beta.2
             </div>
         </div>
 
-        <div class="flex items-center gap-5">
+        <div class="flex items-center gap-3">
             {{-- Doc search --}}
-            <div class="">
+            <div>
                 <div
                     id="docsearch"
                     x-on:click="if (window.innerWidth < 640) window.scrollTo({ top: 0, behavior: 'instant' })"
@@ -37,62 +37,57 @@
                 <a
                     href="/"
                     @class([
-                        'transition duration-200',
+                        'hidden transition duration-200 lg:block',
                         'font-medium' => request()->routeIs('welcome*'),
                         'opacity-60 hover:opacity-100' => ! request()->routeIs('welcome*'),
                     ])
                 >
                     Home
                 </a>
-                <div class="size-[3px] rotate-45 rounded-sm bg-gray-400"></div>
+                <div
+                    class="hidden size-[3px] rotate-45 rounded-sm bg-gray-400 lg:block"
+                ></div>
                 <a
                     href="{{ route('early-adopter') }}"
                     @class([
-                        'transition duration-200',
+                        'hidden transition duration-200 lg:block',
                         'font-medium' => request()->routeIs('early-adopter*'),
                         'opacity-60 hover:opacity-100' => ! request()->routeIs('early-adopter*'),
                     ])
                 >
                     Mobile
                 </a>
-                <div class="size-[3px] rotate-45 rounded-sm bg-gray-400"></div>
-                <a
-                    href="/docs/"
-                    @class([
-                        'transition duration-200',
-                        'font-medium' => request()->is('docs*'),
-                        'opacity-60 hover:opacity-100' => ! request()->is('docs*'),
-                    ])
-                >
-                    Docs
-                </a>
+                <div
+                    class="hidden size-[3px] rotate-45 rounded-sm bg-gray-400 lg:block"
+                ></div>
+                @if ($hasMenu)
+                    <button
+                        type="button"
+                        @click="showDocsNavigation = !showDocsNavigation"
+                        class="p-2 focus:outline-none focus:ring-0"
+                    >
+                        <div x-show="!showDocsNavigation">
+                            <x-icons.menu
+                                class="h-6 w-6 text-gray-600 dark:text-gray-300"
+                            />
+                        </div>
+                        <div x-show="showDocsNavigation">
+                            <x-icons.close class="h-6 w-6 text-gray-600" />
+                        </div>
+                    </button>
+                @else
+                    <a
+                        href="/docs/"
+                        @class([
+                            'transition duration-200',
+                            'font-medium' => request()->is('docs*'),
+                            'opacity-60 hover:opacity-100' => ! request()->is('docs*'),
+                        ])
+                    >
+                        Docs
+                    </a>
+                @endif
             </div>
-        </div>
-
-        <div class="flex justify-end pl-4 lg:hidden">
-            @if ($hasMenu)
-                <button
-                    type="button"
-                    @click="showDocsNavigation = !showDocsNavigation"
-                >
-                    <div x-show="!showDocsNavigation">
-                        <x-icons.menu
-                            class="h-6 w-6 text-teal-600 dark:text-teal-300"
-                        />
-                    </div>
-                    <div x-show="showDocsNavigation">
-                        <x-icons.close class="h-6 w-6 text-teal-600" />
-                    </div>
-                </button>
-            @else
-                <a
-                    href="{{ route('docs') }}"
-                    class="flex items-center gap-1 text-teal-600 hover:text-teal-800 dark:text-teal-400 dark:hover:text-teal-200"
-                >
-                    <x-icons.book-text class="size-5" />
-                    <span class="text-md hidden sm:inline">Documentation</span>
-                </a>
-            @endif
         </div>
     </div>
 </nav>
