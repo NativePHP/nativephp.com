21f068d604b82854922e189dbf476f5f5e78ea31
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-20 02:00:46 +0330

    ✨ Update dark mode styling for sponsor components to enhance visual consistency

--- resources/views/components/sponsors/lists/home/corporate-sponsors.blade.php
+++ resources/views/components/sponsors/lists/home/corporate-sponsors.blade.php
@@ -4,7 +4,7 @@
 >
     <a
         href="https://www.redgalaxy.co.uk/"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
         title="RedGalaxy"
     >
         <x-sponsors.logos.redgalaxy class="h-auto max-h-10 max-w-full" />
@@ -17,7 +17,7 @@
 >
     <a
         href="https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
         title="Sevalla"
     >
         <x-sponsors.logos.sevalla
@@ -30,7 +30,7 @@
     <div x-ref="sponsor" class="opacity-0">
     <a
     href="https://serverauth.com/"
-    class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:ring-1 will-change-transform hover:scale-105 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15 hover:bg-gray-200/70"
+    class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:ring-1 will-change-transform hover:scale-105 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15 hover:bg-gray-200/70"
     title="ServerAuth sponsor"
     >
     <x-sponsors.logos.serverauth
@@ -46,7 +46,7 @@
 >
     <a
         href="https://www.kaashosting.nl/?lang=en"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
         title="KaasHosting"
     >
         <x-sponsors.logos.kaashosting
@@ -61,7 +61,7 @@
 >
     <a
         href="https://borah.digital/"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 will-change-transform hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
         title="Borah Digital Labs"
     >
         <x-sponsors.logos.borah
--- resources/views/components/sponsors/lists/home/featured-sponsors.blade.php
+++ resources/views/components/sponsors/lists/home/featured-sponsors.blade.php
@@ -5,7 +5,7 @@
     <a
         href="https://beyondco.de/?utm_source=nativephp&utm_medium=logo&utm_campaign=nativephp"
         title="BeyondCode sponsor"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
     >
         <img
             src="/img/sponsors/beyondcode.png"
@@ -27,7 +27,7 @@
     <a
         href="https://laradevs.com/?ref=nativephp"
         title="Laradevs sponsor"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-[#181a25]/70 dark:hover:ring-white/15"
+        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:scale-105 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-gray-900/40 dark:hover:bg-gray-900/60 dark:hover:ring-white/15"
     >
         <x-sponsors.logos.laradevs
             class="block h-auto max-h-10 max-w-full text-black dark:text-white"
