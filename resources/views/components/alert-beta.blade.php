24f5047c40bc396280abc6908705d6c4c2911ee2
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-21 03:00:32 +0330

    ✨ Update beta alert component with new design and add alert diamond icon for improved visibility

--- resources/views/components/alert-beta.blade.php
+++ resources/views/components/alert-beta.blade.php
@@ -1,26 +1,33 @@
-<div {{ $attributes->class(['rounded-lg flex items-center p-3 mt-8 space-x-6 border
-                text-orange-800 border-orange-300 bg-orange-50
-                dark:text-orange-100 dark:bg-orange-900/20 dark:border-orange-900']) }}>
-    <x-heroicon-o-shield-exclamation class="size-10 ml-3"/>
-    <div>
-        <p>
-            NativePHP is currently in
-            <span
-                class="font-bold italic font-mono flex-inline px-1 mx-0.5 text-base bg-orange-200 dark:bg-orange-900
-                    rounded inline-block"
-            >
-                beta
-            </span>
-        </p>
+<div
+    class="relative z-0 mt-5 flex items-center gap-6 overflow-hidden rounded-2xl bg-yellow-50/50 px-6 py-5 ring-1 ring-black/5 dark:bg-gray-900/40"
+>
+    <div
+        class="absolute left-0 top-1/2 -z-10 size-16 -translate-y-1/2 rounded-full bg-yellow-400/50 blur-2xl dark:block"
+    ></div>
 
-        <a href="/docs/getting-started/status"
+    {{-- Icon --}}
+    <x-icons.alert-diamond class="size-10 shrink-0" />
+
+    {{-- Content --}}
+    <div class="flex flex-col items-start gap-3">
+        <div class="space-y-1">
+            {{-- Title --}}
+            <h5 class="font-medium">Heads up! NativePHP is still in Beta.</h5>
+            {{-- Message --}}
+            <p class="text-sm opacity-50">
+                Be part of the progress! Feedback from awesome users like you
+                gets us closer to perfection.
+            </p>
+        </div>
+
+        {{-- Link --}}
+        <a
+            href="/docs/getting-started/status"
             onclick="fathom.trackEvent('beta_interest');"
-            class="group mt-4 font-bold inline-flex items-center rounded-md px-3 py-1
-                bg-orange-200 hover:bg-orange-300
-                dark:bg-orange-900 dark:hover:bg-orange-500 dark:text-orange-100">
-            Let's get to v1!
-            <x-heroicon-o-rocket-launch class="ml-2 size-5 group-hover:hidden"/>
-            <x-heroicon-s-rocket-launch class="hidden ml-2 size-5 group-hover:block"/>
+            class="inline-flex items-center gap-2 rounded-xl bg-yellow-400/50 py-3 pl-3 pr-5 text-sm transition duration-300 ease-in-out hover:bg-yellow-400/60 dark:bg-[#ffd057] dark:text-black dark:hover:bg-[#ffd057]/90"
+        >
+            <x-icons.rocket class="size-5" />
+            <div>Let's get to v1!</div>
         </a>
     </div>
 </div>
