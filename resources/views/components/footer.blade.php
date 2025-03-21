3ef612e5afb628beeb8c076616233ac0bf189b11
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-21 14:47:18 +0330

    ✨ Enhance footer accessibility by updating aria attributes and improving semantic structure

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,32 @@
 <footer
     class="2xl:max-w-8xl mx-auto max-w-5xl px-5 pb-5 pt-20 xl:max-w-7xl"
-    aria-label="Site footer"
+    aria-labelledby="footer-heading"
 >
     <div class="flex flex-wrap items-center justify-between gap-6">
+        {{-- Logo --}}
         <a
             href="/"
             class="transition duration-200 will-change-transform hover:scale-[1.02]"
-            aria-label="Return to homepage"
+            aria-label="NativePHP homepage"
         >
             <x-logo
                 class="h-5"
                 aria-hidden="true"
+                alt="NativePHP Logo"
             />
             <span class="sr-only">NativePHP homepage</span>
         </a>
-        <div
+
+        {{-- Social links --}}
+        <nav
             class="flex flex-wrap items-center justify-center gap-2.5"
             aria-label="Social networks"
         >
             <x-social-networks-all />
-        </div>
+        </nav>
     </div>
+
+    {{-- Divider --}}
     <div
         class="flex items-center pb-3 pt-5"
         aria-hidden="true"
@@ -29,8 +35,11 @@
         <div class="h-0.5 w-full bg-gray-200/90 dark:bg-[#242734]"></div>
         <div class="size-1.5 rotate-45 bg-gray-200/90 dark:bg-[#242734]"></div>
     </div>
-    <div
+
+    {{-- Copyright --}}
+    <section
         class="flex flex-wrap items-center justify-between gap-x-5 gap-y-3 text-sm text-gray-500"
+        aria-label="Credits and copyright information"
     >
         <div>
             <span>Logo by</span>
@@ -38,7 +47,7 @@
                 href="https://twitter.com/caneco"
                 target="_blank"
                 class="transition duration-200 hover:text-black"
-                aria-label="Caneco on Twitter"
+                aria-label="Caneco's Twitter profile"
                 rel="noopener noreferrer"
             >
                 Caneco
@@ -51,7 +60,7 @@
                 href="https://twitter.com/marcelpociot"
                 target="_blank"
                 class="transition duration-200 hover:text-black"
-                aria-label="Marcel Pociot on Twitter"
+                aria-label="Marcel Pociot's Twitter profile"
                 rel="noopener noreferrer"
             >
                 Marcel Pociot
@@ -61,12 +70,12 @@
                 href="https://twitter.com/simonhamp"
                 target="_blank"
                 class="transition duration-200 hover:text-black"
-                aria-label="Simon Hamp on Twitter"
+                aria-label="Simon Hamp's Twitter profile"
                 rel="noopener noreferrer"
             >
                 Simon Hamp
             </a>
             <span>.</span>
         </div>
-    </div>
+    </section>
 </footer>
