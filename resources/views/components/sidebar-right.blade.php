7fddbaff70006aab19ab2e26f8d4713d4faffb6b
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-25 18:32:49 +0330

    ✨ Update layout and styling for various components to enhance responsiveness and visual consistency

--- resources/views/components/sidebar-right.blade.php
+++ resources/views/components/sidebar-right.blade.php
@@ -1,5 +1,5 @@
 <aside
-    class="sticky top-20 hidden max-h-[calc(100dvh-8rem)] w-64 shrink-0 overflow-y-auto overflow-x-hidden py-4 pl-10 pr-2 xl:block"
+    class="sticky top-20 hidden max-h-[calc(100dvh-8rem)] w-[17rem] shrink-0 overflow-y-auto overflow-x-hidden px-3 py-4 xl:block"
 >
     {{ $slot }}
 </aside>
