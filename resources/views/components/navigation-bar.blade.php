3e18722751bd8262d60e0b70604f6f892f071117
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-03-25 16:05:11 +0330

    ✨ Adjust gap spacing in navigation bar for improved layout consistency

--- resources/views/components/navigation-bar.blade.php
+++ resources/views/components/navigation-bar.blade.php
@@ -29,7 +29,7 @@
         </div>
 
         {{-- Right side --}}
-        <div class="flex items-center gap-3">
+        <div class="flex items-center gap-3.5">
             {{-- Doc search --}}
             <div>
                 <div
