423a20bf4292de2f646f958b9f8159a3ffcb4de9
Simon Hamp <simon.hamp@me.com>
2025-03-26 10:08:44 +0000

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/docsearch.css'],
             refresh: true,
         }),
     ],
