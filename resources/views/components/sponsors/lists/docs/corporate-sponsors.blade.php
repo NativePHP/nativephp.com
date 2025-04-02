f71a6036f33cad46d1b0b4a3a448e8bbff3585fe
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-04-02 23:07:32 +0330

    ✨ Refactor table of contents and sponsors layout; remove unused briefcase icon

--- resources/views/components/sponsors/lists/docs/corporate-sponsors.blade.php
+++ resources/views/components/sponsors/lists/docs/corporate-sponsors.blade.php
@@ -1,71 +1,69 @@
-<a
-    href="https://www.redgalaxy.co.uk/"
-    class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 will-change-transform hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
-    title="Learn more about RedGalaxy"
-    aria-label="Visit RedGalaxy website"
-    rel="noopener"
->
-    <x-sponsors.logos.redgalaxy
-        class="h-auto max-h-8 max-w-full"
-        alt="RedGalaxy logo"
-    />
-    <span class="sr-only">RedGalaxy</span>
-</a>
+@php
+    $sponsors = [
+        [
+            'url' => 'https://www.redgalaxy.co.uk/',
+            'title' => 'Learn more about RedGalaxy',
+            'label' => 'Visit RedGalaxy website',
+            'component' => 'sponsors.logos.redgalaxy',
+            'class' => 'h-auto max-h-8 max-w-full',
+            'alt' => 'RedGalaxy logo',
+            'name' => 'RedGalaxy',
+        ],
+        [
+            'url' => 'https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage',
+            'title' => 'Learn more about Sevalla',
+            'label' => 'Visit Sevalla website',
+            'component' => 'sponsors.logos.sevalla',
+            'class' => 'h-auto max-h-8 max-w-full text-black dark:text-white',
+            'alt' => 'Sevalla logo',
+            'name' => 'Sevalla',
+        ],
+        [
+            'url' => 'https://www.kaashosting.nl/?lang=en',
+            'title' => 'Learn more about KaasHosting',
+            'label' => 'Visit KaasHosting website',
+            'component' => 'sponsors.logos.kaashosting',
+            'class' => 'block h-auto max-h-8 max-w-full fill-[#042340] dark:fill-white',
+            'alt' => 'KaasHosting logo',
+            'name' => 'KaasHosting',
+        ],
+        // Uncomment these if you want to include the commented sponsors
+        /*
+                                    [
+                                        'url' => 'https://serverauth.com/',
+                                        'title' => 'Learn more about ServerAuth',
+                                        'label' => 'Visit ServerAuth website',
+                                        'component' => 'sponsors.logos.serverauth',
+                                        'class' => 'block h-auto max-h-8 max-w-full fill-[#042340] dark:fill-white',
+                                        'alt' => 'ServerAuth logo',
+                                        'name' => 'ServerAuth'
+                                    ],
+                                    [
+                                        'url' => 'https://borah.digital/',
+                                        'title' => 'Learn more about Borah Digital Labs',
+                                        'label' => 'Visit Borah Digital Labs website',
+                                        'component' => 'sponsors.logos.borah',
+                                        'class' => 'block h-auto max-h-8 max-w-full dark:brightness-125',
+                                        'alt' => 'Borah Digital Labs logo',
+                                        'name' => 'Borah Digital Labs'
+                                    ],
+                                */
+    ];
+
+    $randomSponsor = $sponsors[array_rand($sponsors)];
+@endphp
 
 <a
-    href="https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage"
+    href="{{ $randomSponsor['url'] }}"
     class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 will-change-transform hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
-    title="Learn more about Sevalla"
-    aria-label="Visit Sevalla website"
+    title="{{ $randomSponsor['title'] }}"
+    aria-label="{{ $randomSponsor['label'] }}"
     rel="noopener"
 >
-    <x-sponsors.logos.sevalla
-        class="h-auto max-h-8 max-w-full text-black dark:text-white"
-        alt="Sevalla logo"
+    <x-dynamic-component
+        :component="$randomSponsor['component']"
+        class="{{ $randomSponsor['class'] }}"
+        alt="{{ $randomSponsor['alt'] }}"
     />
-    <span class="sr-only">Sevalla</span>
+    <span class="sr-only">{{ $randomSponsor['name'] }}</span>
 </a>
-
-{{--
-    <a
-    href="https://serverauth.com/"
-    class="dark:bg-mirage dark:hover:ring-cloud dark:hover:bg-haiti inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 will-change-transform hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60"
-    title="ServerAuth sponsor"
-    aria-label="Visit ServerAuth website, sponsor"
-    >
-    <x-sponsors.logos.serverauth
-    class="block h-auto max-h-8 max-w-full fill-[#042340] dark:fill-white"
-    />
-    <span class="sr-only">Visit ServerAuth</span>
-    </a>
---}}
-
-<a
-    href="https://www.kaashosting.nl/?lang=en"
-    class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 will-change-transform hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
-    title="Learn more about KaasHosting"
-    aria-label="Visit KaasHosting website"
-    rel="noopener"
->
-    <x-sponsors.logos.kaashosting
-        class="block h-auto max-h-8 max-w-full fill-[#042340] dark:fill-white"
-        alt="KaasHosting logo"
-    />
-    <span class="sr-only">KaasHosting</span>
-</a>
-
-{{--
-<a
-    href="https://borah.digital/"
-    class="inline-grid h-16 w-full shrink-0 place-items-center rounded-2xl bg-gray-100 px-5 transition duration-200 will-change-transform hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
-    title="Learn more about Borah Digital Labs"
-    aria-label="Visit Borah Digital Labs website"
-    rel="noopener"
->
-    <x-sponsors.logos.borah
-        class="block h-auto max-h-8 max-w-full dark:brightness-125"
-        alt="Borah Digital Labs logo"
-    />
-    <span class="sr-only">Borah Digital Labs</span>
-</a>
---}}
--- resources/views/components/toc-and-sponsors.blade.php
+++ resources/views/components/toc-and-sponsors.blade.php
@@ -1,5 +1,28 @@
+<div class="max-w-52">
+    {{-- Sponsors --}}
+    <h3 class="flex items-center gap-1.5 text-sm opacity-60">
+        {{-- Icon --}}
+        <x-icons.star-circle class="size-[18px]" />
+
+        {{-- Label --}}
+        <div>Sponsors</div>
+    </h3>
+
+    {{-- List --}}
+    <div class="space-y-3 pt-2.5">
+        <x-sponsors.lists.docs.featured-sponsors />
+    </div>
+
+    {{-- List --}}
+    <div class="space-y-3 pt-2.5">
+        <x-sponsors.lists.docs.corporate-sponsors />
+    </div>
+</div>
+
 {{-- On this page --}}
-<h3 class="inline-flex items-center gap-1.5 text-sm opacity-60">
+<h3
+    class="mt-5 flex items-center gap-1.5 border-t border-t-black/20 pt-5 text-sm opacity-60 dark:border-t-white/15"
+>
     {{-- Icon --}}
     <x-icons.stacked-lines class="size-[18px]" />
 
@@ -10,7 +33,7 @@
 {{-- Table of contents --}}
 @if (count($tableOfContents) > 0)
     <div
-        class="mt-2 flex max-h-96 flex-col space-y-2 overflow-y-auto overflow-x-hidden border-l text-xs dark:border-l-white/15"
+        class="mt-4 flex max-h-[clamp(11rem,50vh,35vh)] flex-col space-y-2 overflow-y-auto overflow-x-hidden border-l text-xs dark:border-l-white/15"
     >
         @foreach ($tableOfContents as $item)
             <a
@@ -26,48 +49,3 @@
         @endforeach
     </div>
 @endif
-
-<div class="max-w-52">
-    {{-- Sponsor us --}}
-    <a
-        href="https://github.com/nativephp/laravel?sponsor=1"
-        class="mt-10 flex w-full items-center justify-center gap-3 rounded-xl bg-pink-100 p-5 text-center transition duration-300 ease-in-out hover:bg-pink-200/80 dark:bg-pink-600/10 dark:hover:bg-pink-600/15"
-    >
-        <x-icons.love-baloons class="w-8" />
-        <div class="text-sm font-medium dark:font-normal">
-            Sponsor us on GitHub
-        </div>
-    </a>
-
-    {{-- Featured sponsors --}}
-    <h3
-        class="mt-7 flex items-center gap-1.5 border-t border-t-black/20 pt-5 text-sm opacity-60 dark:border-t-white/15"
-    >
-        {{-- Icon --}}
-        <x-icons.star-circle class="size-[18px]" />
-
-        {{-- Label --}}
-        <div>Featured sponsors</div>
-    </h3>
-
-    {{-- List --}}
-    <div class="space-y-3 pt-2.5">
-        <x-sponsors.lists.docs.featured-sponsors />
-    </div>
-
-    {{-- Corporate sponsors --}}
-    <h3
-        class="mt-7 flex items-center gap-1.5 border-t border-t-black/20 pt-5 text-sm opacity-60 dark:border-t-white/15"
-    >
-        {{-- Icon --}}
-        <x-icons.briefcase class="size-[18px]" />
-
-        {{-- Label --}}
-        <div>Corporate sponsors</div>
-    </h3>
-
-    {{-- List --}}
-    <div class="space-y-3 pt-2.5">
-        <x-sponsors.lists.docs.corporate-sponsors />
-    </div>
-</div>
