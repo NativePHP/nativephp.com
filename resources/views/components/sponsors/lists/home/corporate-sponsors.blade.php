e432af39a3935cf3e56e7676b2c2bff696427dc8
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-04-03 01:07:55 +0330

    ✨ Update corporate sponsors layout; reduce height of sponsor links and logos for improved design consistency

--- resources/views/components/sponsors/lists/home/corporate-sponsors.blade.php
+++ resources/views/components/sponsors/lists/home/corporate-sponsors.blade.php
@@ -4,13 +4,13 @@
 >
     <a
         href="https://www.redgalaxy.co.uk/"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
+        class="inline-grid h-16 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
         title="Learn more about RedGalaxy"
         aria-label="Visit RedGalaxy website"
         rel="noopener"
     >
         <x-sponsors.logos.redgalaxy
-            class="h-auto max-h-10 max-w-full"
+            class="h-auto max-h-5 max-w-full"
             alt="RedGalaxy logo"
             loading="lazy"
         />
@@ -24,13 +24,13 @@
 >
     <a
         href="https://sevalla.com/?utm_source=nativephp&utm_medium=Referral&utm_campaign=homepage"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
+        class="inline-grid h-16 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
         title="Learn more about Sevalla"
         aria-label="Visit Sevalla website"
         rel="noopener"
     >
         <x-sponsors.logos.sevalla
-            class="h-auto max-h-10 max-w-full text-black dark:text-white"
+            class="h-auto max-h-5 max-w-full text-black dark:text-white"
             alt="Sevalla logo"
             loading="lazy"
         />
@@ -42,12 +42,12 @@
     <div x-ref="sponsor" class="opacity-0">
     <a
     href="https://serverauth.com/"
-    class="dark:bg-mirage dark:hover:ring-cloud dark:hover:bg-haiti inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60"
+    class="dark:bg-mirage dark:hover:ring-cloud dark:hover:bg-haiti inline-grid h-16 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60"
     title="Learn m"
     aria-label="Visit ServerAuth website"
     >
     <x-sponsors.logos.serverauth
-    class="block h-auto max-h-10 max-w-full fill-[#042340] dark:fill-white"
+    class="block h-auto max-h-5 max-w-full fill-[#042340] dark:fill-white"
     alt="ServerAuth logo"
     />
     <span class="sr-only">ServerAuth</span>
@@ -61,13 +61,13 @@
 >
     <a
         href="https://www.kaashosting.nl/?lang=en"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
+        class="inline-grid h-16 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
         title="Learn more about KaasHosting"
         aria-label="Visit KaasHosting website"
         rel="noopener"
     >
         <x-sponsors.logos.kaashosting
-            class="block h-auto max-h-10 max-w-full fill-[#042340] dark:fill-white"
+            class="block h-auto max-h-5 max-w-full fill-[#042340] dark:fill-white"
             alt="KaasHosting logo"
             loading="lazy"
         />
@@ -76,23 +76,23 @@
 </div>
 
 {{--
-<div
+    <div
     x-ref="sponsor"
     class="opacity-0"
->
-    <a
-        href="https://borah.digital/"
-        class="inline-grid h-32 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
-        title="Learn more about Borah Digital Labs"
-        aria-label="Visit Borah Digital Labs website"
-        rel="noopener"
     >
-        <x-sponsors.logos.borah
-            class="block h-auto max-h-10 max-w-full fill-[#042340] dark:fill-white"
-            alt="Borah Digital Labs logo"
-            loading="lazy"
-        />
-        <span class="sr-only">Borah Digital Labs</span>
+    <a
+    href="https://borah.digital/"
+    class="inline-grid h-16 w-60 shrink-0 place-items-center rounded-2xl bg-gray-100 p-5 transition duration-200 hover:bg-gray-200/70 hover:ring-1 hover:ring-black/60 dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud"
+    title="Learn more about Borah Digital Labs"
+    aria-label="Visit Borah Digital Labs website"
+    rel="noopener"
+    >
+    <x-sponsors.logos.borah
+    class="block h-auto max-h-5 max-w-full fill-[#042340] dark:fill-white"
+    alt="Borah Digital Labs logo"
+    loading="lazy"
+    />
+    <span class="sr-only">Borah Digital Labs</span>
     </a>
-</div>
+    </div>
 --}}
