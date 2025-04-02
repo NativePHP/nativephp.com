0f86162955ab50544914d5dfd447beef268277ce
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-04-02 20:58:46 +0330

    ✨ Refactor navigation components to improve layout and transition effects

--- resources/views/components/sidebar-left-navigation.blade.php
+++ resources/views/components/sidebar-left-navigation.blade.php
@@ -8,7 +8,7 @@
     </nav>
 </aside>
 
-<nav
+<div
     x-show="showDocsNavigation"
     x-transition:enter="transition duration-200 ease-out"
     x-transition:enter-start="translate-y-1 opacity-0"
@@ -16,13 +16,13 @@
     x-transition:leave="transition duration-150 ease-in"
     x-transition:leave-start="translate-y-0 opacity-100"
     x-transition:leave-end="translate-y-1 opacity-0"
-    class="fixed left-0 top-20 z-40 h-screen w-full overflow-y-auto bg-white px-4 pb-16 pt-10 dark:bg-mirage"
+    class="fixed inset-0 z-40 h-screen w-screen overflow-y-auto bg-white dark:bg-mirage"
 >
-    <x-platform-switcher />
-
-    {!! $slot !!}
-
-    <div class="my-16 flex items-center justify-center space-x-6">
-        <x-social-networks-all />
-    </div>
-</nav>
+    <nav class="px-5 pt-24">
+        <x-platform-switcher />
+        {!! $slot !!}
+        <div class="my-16 flex items-center justify-center space-x-6">
+            <x-social-networks-all />
+        </div>
+    </nav>
+</div>
