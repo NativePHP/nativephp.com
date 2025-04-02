d0a563d3c49a6a970ee20669b17f4d0e016ba738
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-04-02 20:08:51 +0330

    ✨ Update layout components to remove unnecessary max-height constraints

--- resources/views/components/layout-three-columns.blade.php
+++ resources/views/components/layout-three-columns.blade.php
@@ -1,7 +1,7 @@
 <x-layout :hasMenu="! empty($sidebarLeft)">
     {{-- Main container --}}
     <main
-        class="mx-auto flex w-full max-w-5xl grow px-4 pt-1 xl:max-w-7xl 2xl:max-w-[90rem]"
+        class="mx-auto flex w-full max-w-5xl grow items-start px-4 pt-1 xl:max-w-7xl 2xl:max-w-[90rem]"
     >
         {{-- Left sidebar --}}
         @if (! empty($sidebarLeft))
@@ -10,7 +10,7 @@
             </x-sidebar-left-navigation>
         @endif
 
-        <div class="flex w-full min-w-0 grow px-2 pt-2">
+        <div class="flex w-full min-w-0 grow items-start px-2 pt-2">
             {{-- Content --}}
             <article class="flex w-full min-w-0 grow flex-col lg:pl-5 xl:pr-5">
                 {{ $slot }}
