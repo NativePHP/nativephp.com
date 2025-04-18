e88b4cb0d5626a3f3b60751b5b4805a6180d62ba
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2025-04-18 11:30:34 -0400

    remove local testing code

--- app/Http/Controllers/OrderSuccessController.php
+++ app/Http/Controllers/OrderSuccessController.php
@@ -10,13 +10,6 @@ class OrderSuccessController extends Controller
 {
     public function __invoke(Request $request, string $checkoutSessionId)
     {
-        if (app()->isLocal()) {
-            return view('order-success', [
-                'email' => $checkoutSessionId === 'withkey' ? 'user@example.com' : null,
-                'licenseKey' => $checkoutSessionId === 'withkey' ? '3715203a-8305-4fab-8ff6-e52a31c409ab' : null,
-            ]);
-        }
-
         $stripe = app(StripeClient::class);
         $checkoutSession = $stripe->checkout->sessions->retrieve($checkoutSessionId);
 
