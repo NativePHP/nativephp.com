e6b8507219a799665cf95cc4cf95f2c40b52681a
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2025-04-19 13:53:08 -0400

    optimize email and license key handling in OrderSuccessController

--- app/Http/Controllers/OrderSuccessController.php
+++ app/Http/Controllers/OrderSuccessController.php
@@ -10,22 +10,48 @@ class OrderSuccessController extends Controller
 {
     public function __invoke(Request $request, string $checkoutSessionId)
     {
-        $stripe = app(StripeClient::class);
-        $checkoutSession = $stripe->checkout->sessions->retrieve($checkoutSessionId);
+        $email = $this->getEmail($request, $checkoutSessionId);
 
-        if (! $checkoutSession) {
-            return to_route('early-adopter');
-        }
-
-        $request->session()->put('customer_email', $email = $checkoutSession->customer_details->email);
-
-        if ($licenseKey = Cache::get($email.'.license_key')) {
-            $request->session()->put('license_key', $licenseKey);
-        }
+        $licenseKey = $this->getLicenseKey($request, $email);
 
         return view('order-success', [
             'email' => $email,
             'licenseKey' => $licenseKey,
         ]);
+    }
+
+    private function getEmail(Request $request, string $checkoutSessionId): ?string
+    {
+        if ($email = $request->session()->get('customer_email')) {
+            return $email;
+        }
+
+        $stripe = app(StripeClient::class);
+        $checkoutSession = $stripe->checkout->sessions->retrieve($checkoutSessionId);
+
+        if (! ($email = $checkoutSession?->customer_details?->email)) {
+            return null;
+        }
+
+        $request->session()->put('customer_email', $email);
+
+        return $email;
+    }
+
+    private function getLicenseKey(Request $request, ?string $email): ?string
+    {
+        if ($licenseKey = $request->session()->get('license_key')) {
+            return $licenseKey;
+        }
+
+        if (! $email) {
+            return null;
+        }
+
+        if ($licenseKey = Cache::get($email.'.license_key')) {
+            $request->session()->put('license_key', $licenseKey);
+        }
+
+        return $licenseKey;
     }
 }
