2152d53368c77dd09e23b6e74bb9e251a1f1b0ae
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2025-05-01 11:58:28 -0400

    remove unnecessary stripe service config & binding

--- config/services.php
+++ config/services.php
@@ -31,14 +31,6 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'stripe' => [
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-        'webhook' => [
-            'secret' => env('STRIPE_WEBHOOK_SECRET'),
-        ],
-    ],
-
     'anystack' => [
         'key' => env('ANYSTACK_API_KEY'),
     ],
