7a39fd2eaafef623437677293438fc8ffce1b201
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-05-09 18:57:06 +0330

    💄 Fix layout and spacing in EAP banner countdown component

--- resources/views/components/eap-banner.blade.php
+++ resources/views/components/eap-banner.blade.php
@@ -1,7 +1,7 @@
 <a
     href="/mobile"
     onclick="fathom.trackEvent('alert_click');"
-    class="group relative z-30 flex flex-col items-center justify-center gap-2.5 bg-gradient-to-tl from-[#211d3a] to-[#6f64c3] px-5 py-3 md:flex-row"
+    class="group relative z-30 flex flex-col items-center justify-center gap-x-3 gap-y-2.5 bg-gradient-to-tl from-[#211d3a] to-[#6f64c3] px-5 py-3 md:flex-row"
 >
     <div class="flex items-center justify-center gap-3">
         {{-- Text --}}
@@ -26,26 +26,34 @@
         </div>
         {{-- Arrow --}}
         <x-icons.right-arrow
-            class="size-3 shrink-0 text-white transition duration-200 ease-in-out will-change-transform group-hover:translate-x-1"
+            class="size-3 shrink-0 text-white transition duration-200 ease-in-out will-change-transform group-hover:translate-x-0.5"
         />
     </div>
 
     {{-- Countdown --}}
     <div
         x-data="countdown('2025-05-31T23:59:59')"
-        class="flex items-center gap-1.5 rounded-lg px-2.5 py-1 text-sm text-white ring-1 ring-white/45 md:text-base"
+        class="flex items-center gap-1 rounded-lg px-2.5 py-1 text-sm text-white ring-1 ring-white/45 md:text-base"
     >
         <div class="pr-1 opacity-70">Ends in</div>
-        <number-flow x-ref="dd"></number-flow>
-        <div class="-ml-1 pt-0.5 text-sm opacity-70">d</div>
+        <div class="flex min-w-8 justify-center gap-0.5">
+            <number-flow x-ref="dd"></number-flow>
+            <div class="pt-[3px] text-sm opacity-70">d</div>
+        </div>
         <div>:</div>
-        <number-flow x-ref="hh"></number-flow>
-        <div class="-ml-1 pt-0.5 text-sm opacity-70">h</div>
+        <div class="flex min-w-8 justify-center gap-0.5">
+            <number-flow x-ref="hh"></number-flow>
+            <div class="pt-[3px] text-sm opacity-70">h</div>
+        </div>
         <div>:</div>
-        <number-flow x-ref="mm"></number-flow>
-        <div class="-ml-1 pt-0.5 text-sm opacity-70">m</div>
+        <div class="flex min-w-8 justify-center gap-0.5">
+            <number-flow x-ref="mm"></number-flow>
+            <div class="pt-[3px] text-sm opacity-70">m</div>
+        </div>
         <div>:</div>
-        <number-flow x-ref="ss"></number-flow>
-        <div class="-ml-1 pt-0.5 text-sm opacity-70">s</div>
+        <div class="flex min-w-8 justify-center gap-0.5">
+            <number-flow x-ref="ss"></number-flow>
+            <div class="pt-[3px] text-sm opacity-70">s</div>
+        </div>
     </div>
 </a>
