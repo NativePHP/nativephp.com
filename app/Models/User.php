feae8f31532d2dc7afe469fdef7e893e2fbe63a2
Steven Fox <62109327+steven-fox@users.noreply.github.com>
2025-05-14 10:01:05 -0400

    create License model and relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
@@ -24,4 +25,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * @return HasMany<License>
+     */
+    public function licenses(): HasMany
+    {
+        return $this->hasMany(License::class);
+    }
 }
---
+++ database/factories/LicenseFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Subscription;
+use App\Models\License;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Date;
+use Laravel\Cashier\SubscriptionItem;
+
+/**
+ * @extends Factory<License>
+ */
+class LicenseFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'subscription_item_id' => SubscriptionItem::factory(),
+            'policy_name' => fake()->randomElement(Subscription::cases())->value,
+            'key' => fake()->uuid(),
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            'updated_at' => fn (array $attrs) => $attrs['created_at'],
+            'expires_at' => fn (array $attrs) => Date::parse($attrs['created_at'])->addYear(),
+        ];
+    }
+}
