18520b296af0acf3613aa0a09952b082cd89f2fb
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-06-05 20:35:05 +0330

    ✨ Enhance mobile menu and social links styling for improved accessibility and user experience

--- resources/views/components/social-networks-all.blade.php
+++ resources/views/components/social-networks-all.blade.php
@@ -2,7 +2,7 @@
     <a
         href="https://x.com/nativephp"
         title="Twitter"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.twitter
             class="size-[1.1rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -14,7 +14,7 @@
     <a
         href="https://youtube.com/@NativePHPOfficial"
         title="Youtube"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.youtube
             class="size-[1.4rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -26,7 +26,7 @@
     <a
         href="https://bsky.app/profile/nativephp.com"
         title="Bluesky"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.bluesky
             class="size-[1.1rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -38,7 +38,7 @@
     <a
         href="https://discord.gg/X62tWNStZK"
         title="Go to discord server"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.discord
             class="size-[1.1rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -50,7 +50,7 @@
     <a
         href="https://opencollective.com/nativephp"
         title="NativePHP on Open Collective"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.opencollective
             class="size-[1.1rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -62,7 +62,7 @@
     <a
         href="https://github.com/nativephp"
         title="Source code of NativePHP"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.github
             class="size-[1.1rem] transition duration-200 group-hover:fill-violet-400 dark:fill-white"
@@ -74,7 +74,7 @@
     <a
         href="https://pinkary.com/@nativephp"
         title="NativePHP on Pinkary"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.pinkary
             class="size-[1.6rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
@@ -86,7 +86,7 @@
     <a
         href="https://www.linkedin.com/company/nativephp/"
         title="NativePHP on LinkedIn"
-        class="group inline-grid size-10 place-items-center rounded-xl bg-gray-100 transition duration-200 hover:bg-gray-200/70 dark:bg-mirage dark:hover:bg-haiti"
+        class="group dark:hover:bg-haiti inline-grid size-10 place-items-center rounded-xl bg-zinc-200/80 transition duration-200 hover:bg-gray-200/70 dark:bg-gray-700/40"
     >
         <x-icons.linkedin
             class="size-[1.1rem] text-black transition duration-200 group-hover:text-violet-400 dark:text-white"
