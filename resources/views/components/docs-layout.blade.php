e7e4cf338f4b4a6cbcc3d029fdc430efeb3df6ec
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-08-21 22:03:41 +0330

    🎨 Refactor documentation components and update structure

--- resources/views/components/docs-layout.blade.php
+++ resources/views/components/docs-layout.blade.php
@@ -5,18 +5,18 @@
     >
         {{-- Left sidebar --}}
         @if (! empty($sidebarLeft))
-            <x-sidebar-left-navigation>
+            <x-docs.sidebar-left-navigation>
                 {{ $sidebarLeft }}
-            </x-sidebar-left-navigation>
+            </x-docs.sidebar-left-navigation>
         @endif
 
         <div class="flex w-full min-w-0 grow items-start px-2 pt-2">
             {{-- Content --}}
             <article class="flex w-full min-w-0 grow flex-col lg:pl-5 xl:pr-5">
                 {{-- Docs mobile menu --}}
-                <x-docs-menu>
+                <x-docs.menu>
                     <nav class="docs-navigation">{{ $sidebarLeft }}</nav>
-                </x-docs-menu>
+                </x-docs.menu>
 
                 {{-- Main content --}}
                 <div class="mt-3">{{ $slot }}</div>
@@ -24,9 +24,9 @@
 
             {{-- Right sidebar --}}
             @if (! empty($sidebarRight))
-                <x-sidebar-right>
+                <x-docs.sidebar-right>
                     {{ $sidebarRight }}
-                </x-sidebar-right>
+                </x-docs.sidebar-right>
             @endif
         </div>
     </main>
--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -4,16 +4,16 @@
     </x-slot>
 
     <x-slot name="sidebarRight">
-        <x-toc-and-sponsors :tableOfContents="$tableOfContents" />
+        <x-docs.toc-and-sponsors :tableOfContents="$tableOfContents" />
     </x-slot>
 
     <h1 class="text-4xl font-semibold">
         {{ $title }}
     </h1>
 
-    <x-separator class="mt-4" />
+    <x-docs.separator class="mt-4" />
 
-    <x-alert-v1-announcement />
+    <x-docs.alert-v1-announcement />
 
     {{-- Table of contents --}}
     <div class="xl:hidden">
@@ -49,18 +49,18 @@
         {!! $content !!}
     </div>
 
-    <x-separator class="mt-8" />
+    <x-docs.separator class="mt-8" />
 
     @php
         $linkAlign = $previousPage === null ? 'right' : 'between';
     @endphp
 
-    <x-flex-list-of-links
+    <x-docs.flex-list-of-links
         align="{{$linkAlign}}"
         class="mt-5"
     >
         @if ($previousPage !== null)
-            <x-link-button href="{{ $previousPage['path'] }}">
+            <x-docs.link-button href="{{ $previousPage['path'] }}">
                 <div class="self-center justify-self-start">
                     <div
                         class="flex items-center justify-start gap-1.5 opacity-60"
@@ -72,11 +72,11 @@
                     </div>
                     <div class="pt-1">{{ $previousPage['title'] }}</div>
                 </div>
-            </x-link-button>
+            </x-docs.link-button>
         @endif
 
         @if ($nextPage !== null)
-            <x-link-button href="{{ $nextPage['path'] }}">
+            <x-docs.link-button href="{{ $nextPage['path'] }}">
                 <div class="self-center justify-self-end">
                     <div
                         class="flex items-center justify-end gap-1.5 opacity-60"
@@ -86,17 +86,17 @@
                     </div>
                     <div class="pt-1">{{ $nextPage['title'] }}</div>
                 </div>
-            </x-link-button>
+            </x-docs.link-button>
         @endif
-    </x-flex-list-of-links>
+    </x-docs.flex-list-of-links>
 
     <div class="pt-5 text-center sm:text-left">
-        <x-link-subtle
+        <x-docs.link-subtle
             href="{{ $editUrl }}"
             target="_blank"
             rel="noopener"
         >
             Edit this page on GitHub
-        </x-link-subtle>
+        </x-docs.link-subtle>
     </div>
 </x-docs-layout>
