cbcec616de42eb16813648a21d6521fc85c68d6b
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2025-08-23 16:23:10 +0330

    💄 Update featured partner card styles and enhance partners section layout

--- resources/views/components/home/featured-partner-card.blade.php
+++ resources/views/components/home/featured-partner-card.blade.php
@@ -16,7 +16,7 @@
     aria-label="Visit {{ $partnerName }} website"
     target="_blank"
     rel="sponsored"
-    class="group flex flex-col rounded-2xl bg-white/50 p-6 transition duration-200 hover:bg-white/70 hover:shadow-lg hover:shadow-gray-200/70 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500/60 dark:bg-slate-950/30 dark:hover:bg-slate-950/50 dark:hover:shadow-transparent"
+    class="group dark:hover:ring-cloud/70 flex flex-col rounded-2xl bg-white/50 p-7 text-pretty transition duration-200 will-change-transform hover:-translate-y-0.5 hover:bg-white/70 hover:shadow-lg hover:ring-1 hover:shadow-gray-200/70 hover:ring-black/10 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500/60 dark:bg-slate-950/30 dark:hover:bg-slate-950/80 dark:hover:shadow-transparent"
 >
     <div class="grid h-15 w-35 place-items-center">{{ $logo }}</div>
 
--- resources/views/components/home/partners.blade.php
+++ resources/views/components/home/partners.blade.php
@@ -15,10 +15,10 @@
             </h3>
         </div>
 
-        <div class="mt-5 flex flex-wrap">
+        <div class="mt-7 flex flex-wrap gap-5">
             {{-- Featured partners --}}
             <div
-                class="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-5"
+                class="grid w-full grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-5"
                 aria-label="Featured partners of the NativePHP project"
             >
                 <x-home.featured-partner-card
@@ -88,6 +88,27 @@
                     </x-slot>
                 </x-home.featured-partner-card>
             </div>
+
+            <div class="flex flex-wrap gap-5">
+                <a
+                    href="/docs/getting-started/sponsoring"
+                    aria-label="Learn about sponsoring the NativePHP project"
+                    class="flex max-w-1/2 items-center gap-5 rounded-xl bg-gradient-to-t from-slate-700 to-slate-950 p-6 text-white"
+                >
+                    <div class="flex flex-col gap-1">
+                        <div class="">
+                            Get more from NativePHP as a partner!
+                        </div>
+                        <p class="text-sm opacity-70">
+                            Our Partners are helping us bring NativePHP to
+                            everyone and getting some incredible benefits to
+                            boot
+                        </p>
+                    </div>
+                </a>
+
+                <div class=""></div>
+            </div>
         </div>
     </div>
     <div class="divide-y divide-[#242A2E]/20 *:py-8">
@@ -144,82 +165,5 @@
                 <x-sponsors.lists.home.corporate-sponsors />
             </div>
         </div>
-    </div>
-    <div
-        x-init="
-            () => {
-                motion.inView(
-                    $el,
-                    (element) => {
-                        motion.animate(
-                            $el,
-                            {
-                                opacity: [0, 1],
-                                x: [-50, 0],
-                            },
-                            {
-                                duration: 0.7,
-                                ease: motion.circOut,
-                            },
-                        )
-                    },
-                    {
-                        amount: 0.5,
-                    },
-                )
-            }
-        "
-        class="opacity-0 will-change-transform"
-    >
-        <a
-            href="/docs/getting-started/sponsoring"
-            class="group dark:bg-mirage dark:hover:bg-haiti dark:hover:ring-cloud flex flex-wrap items-center justify-center gap-x-5 gap-y-3 rounded-3xl bg-gray-100 px-8 py-8 transition duration-200 ease-in-out hover:ring-1 hover:ring-black/60 md:justify-between md:px-12 md:py-10"
-            aria-label="Learn about sponsoring the NativePHP project"
-        >
-            <div
-                class="inline-flex shrink-0 flex-col-reverse items-center gap-x-5 gap-y-3 md:flex-row"
-            >
-                <div class="space-y-2 text-2xl font-medium">
-                    <span>Want</span>
-                    <span>your logo</span>
-                    <span>here?</span>
-                </div>
-                {{-- Arrow --}}
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-12 -rotate-45 transition duration-300 ease-out will-change-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5 md:w-16 md:rotate-0"
-                    aria-hidden="true"
-                >
-                    <path
-                        class="text-[#e8e4f8] dark:text-[#31416e]/30"
-                        fill="currentColor"
-                        d="M12 22c5.5228 0 10 -4.4772 10 -10 0 -5.52285 -4.4772 -10 -10 -10C6.47715 2 2 6.47715 2 12c0 5.5228 4.47715 10 10 10Z"
-                        stroke-width="1"
-                    ></path>
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M14.499 2.49707h7v7"
-                        stroke-width="1"
-                    ></path>
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M21.499 2.49707 5.49902 18.4971"
-                        stroke-width="1"
-                    ></path>
-                </svg>
-            </div>
-            <div
-                class="text-center font-light opacity-50 md:max-w-xs md:text-left md:text-lg"
-            >
-                Become a sponsor and get your logo on our README on GitHub with
-                a link to your site.
-            </div>
-        </a>
     </div>
 </section>
