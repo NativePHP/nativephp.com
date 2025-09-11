301d94664c21b4e1008c4334c26016a258f12c73
Simon Hamp <simon.hamp@me.com>
2025-09-11 12:05:51 +0100

    Organise routes

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,21 @@ use Illuminate\Support\Str;
 |
 */
 
-Route::redirect('/newsletter', 'https://simonhamp.mailcoach.app/nativephp');
-Route::redirect('/phpverse-2025', 'https://lp.jetbrains.com/phpverse-2025');
-Route::redirect('/docs/1/getting-started/sponsoring', '/sponsor');
-Route::redirect('/docs/desktop/1/getting-started/sponsoring', '/sponsor');
-Route::redirect('/discord', 'https://discord.gg/X62tWNStZK');
+Route::redirect('newsletter', 'https://simonhamp.mailcoach.app/nativephp');
+Route::redirect('phpverse-2025', 'https://lp.jetbrains.com/phpverse-2025');
+Route::redirect('docs/1/getting-started/sponsoring', '/sponsor');
+Route::redirect('docs/desktop/1/getting-started/sponsoring', '/sponsor');
+Route::redirect('discord', 'https://discord.gg/X62tWNStZK');
+Route::redirect('bifrost', 'https://bifrost.nativephp.com');
+Route::redirect('mobile', 'pricing');
+Route::redirect('ios', 'pricing');
+Route::redirect('t-shirt', 'pricing');
+Route::redirect('tshirt', 'pricing');
 
 Route::view('/', 'welcome')->name('welcome');
 Route::view('pricing', 'pricing')->name('pricing');
 Route::view('brand', 'brand')->name('brand');
-Route::redirect('mobile', 'pricing');
 Route::view('laracon-us-2025-giveaway', 'laracon-us-2025-giveaway')->name('laracon-us-2025-giveaway');
-Route::redirect('ios', 'pricing');
-Route::redirect('t-shirt', 'pricing');
-Route::redirect('tshirt', 'pricing');
 Route::view('privacy-policy', 'privacy-policy')->name('privacy-policy');
 Route::view('terms-of-service', 'terms-of-service')->name('terms-of-service');
 Route::view('partners', 'partners')->name('partners');
@@ -38,20 +39,20 @@ Route::view('sponsor', 'sponsoring')->name('sponsoring');
 Route::get('blog', [ShowBlogController::class, 'index'])->name('blog');
 Route::get('blog/{article}', [ShowBlogController::class, 'show'])->name('article');
 
-Route::get('/docs/{platform}/{version}/{page}.md', [ShowDocumentationController::class, 'serveRawMarkdown'])
+Route::get('docs/{platform}/{version}/{page}.md', [ShowDocumentationController::class, 'serveRawMarkdown'])
     ->where('page', '(.*)')
     ->where('platform', '[a-z]+')
     ->where('version', '[0-9]+')
     ->name('docs.raw');
 
-Route::get('/docs/{platform}/{version}/{page?}', ShowDocumentationController::class)
+Route::get('docs/{platform}/{version}/{page?}', ShowDocumentationController::class)
     ->where('page', '(.*)')
     ->where('platform', '[a-z]+')
     ->where('version', '[0-9]+')
     ->name('docs.show');
 
 // Forward unversioned requests to the latest version
-Route::get('/docs/{page?}', function ($page = null) {
+Route::get('docs/{page?}', function ($page = null) {
     $page ??= 'introduction';
     $version = session('viewing_docs_version', '1');
     $platform = session('viewing_docs_platform', 'mobile');
@@ -80,4 +81,4 @@ Route::get('/docs/{page?}', function ($page = null) {
     ]);
 })->name('docs')->where('page', '.*');
 
-Route::get('/order/{checkoutSessionId}', App\Livewire\OrderSuccess::class)->name('order.success');
+Route::get('order/{checkoutSessionId}', App\Livewire\OrderSuccess::class)->name('order.success');
