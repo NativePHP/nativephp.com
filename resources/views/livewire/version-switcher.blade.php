724e3762937ae034bf6f96684dc792472fce994e
gwleuverink <willem@leuver.ink>
2025-10-01 00:08:37 +0200

    version switcher - final touches

--- resources/views/livewire/version-switcher.blade.php
+++ resources/views/livewire/version-switcher.blade.php
@@ -1,20 +1,29 @@
-<form wire:submit.prevent>
+<form
+    wire:submit.prevent
+    class="
+        inline-flex items-center
+        opacity-60 hover:opacity-100 transition-opacity duration-200
+        focus-within:outline-2 focus-within:rounded-xs focus-within:outline-offset-2
+    "
+>
 
-    <div class="inline-flex items-center opacity-60 hover:opacity-100 transition-opacity duration-200 focus-within:outline-2 focus-within:rounded-xs focus-within:outline-offset-2">
+    <x-icons.git-branch class="size-4" />
 
-        <x-icons.git-branch class="size-4" />
+    <select
+        name="version"
+        wire:model.live="version"
+        class="
+            -ml-1 py-0 pr-8
+            bg-transparent border-0
+            text-sm whitespace-nowrap
+            focus:ring-0
+        "
+    >
 
-        <select
-            name="version"
-            wire:model.live="version"
-            class="text-sm -ml-1 py-0 pr-8 bg-transparent border-0 whitespace-nowrap focus:ring-0"
-        >
+        @foreach($versions as $number => $label)
+            <option value="{{ $number }}">Version {{ $label }}</option>
+        @endforeach
 
-            @foreach($versions as $number => $label)
-                <option value="{{ $number }}">Version {{ $label }}</option>
-            @endforeach
-
-        </select>
-    </div>
+    </select>
 
 </form>
