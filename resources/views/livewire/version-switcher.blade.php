5a538916e2466d137bd9ac362e3a79a47af37d89
gwleuverink <willem@leuver.ink>
2025-10-01 00:02:43 +0200

    version switcher styling

--- resources/views/livewire/version-switcher.blade.php
+++ resources/views/livewire/version-switcher.blade.php
@@ -1,11 +1,20 @@
-<form wire:submit="switch">
+<form wire:submit.prevent>
 
-    <select wire:model.live="version" name="version">
+    <div class="inline-flex items-center opacity-60 hover:opacity-100 transition-opacity duration-200 focus-within:outline-2 focus-within:rounded-xs focus-within:outline-offset-2">
 
-        @foreach($versions as $number => $label)
-            <option value="{{ $number }}">{{ $label }}</option>
-        @endforeach
+        <x-icons.git-branch class="size-4" />
 
-    </select>
+        <select
+            name="version"
+            wire:model.live="version"
+            class="text-sm -ml-1 py-0 pr-8 bg-transparent border-0 whitespace-nowrap focus:ring-0"
+        >
+
+            @foreach($versions as $number => $label)
+                <option value="{{ $number }}">Version {{ $label }}</option>
+            @endforeach
+
+        </select>
+    </div>
 
 </form>
