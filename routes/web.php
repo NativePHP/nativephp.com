1a5edb0646650908311c980fb41663bc4ee6a435
Simon Hamp <simon.hamp@me.com>
2025-12-11 03:59:59 +0000

    Leads

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::view('laracon-us-2025-giveaway', 'laracon-us-2025-giveaway')->name('larac
 Route::view('privacy-policy', 'privacy-policy')->name('privacy-policy');
 Route::view('terms-of-service', 'terms-of-service')->name('terms-of-service');
 Route::view('partners', 'partners')->name('partners');
+Route::view('build-my-app', 'build-my-app')->name('build-my-app');
 Route::get('plugins', [PluginDirectoryController::class, 'index'])->name('plugins');
 Route::get('plugins/directory', App\Livewire\PluginDirectory::class)->name('plugins.directory');
 Route::view('sponsor', 'sponsoring')->name('sponsoring');
