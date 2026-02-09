0ad5890e06865e691ca6c5831a1efbc6ef353a53
Simon Hamp <simon.hamp@me.com>
2026-02-09 13:49:07 +0000

    Remove unnecessary banner

--- resources/views/license/renewal.blade.php
+++ resources/views/license/renewal.blade.php
@@ -43,26 +43,6 @@
 
                 {{-- Renewal Information --}}
                 <div class="px-4 py-5 sm:px-6 border-t border-gray-200 dark:border-gray-700">
-                    @if($isNearExpiry)
-                        <div class="rounded-md bg-yellow-50 dark:bg-yellow-900/20 p-4 mb-6">
-                            <div class="flex">
-                                <div class="flex-shrink-0">
-                                    <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div class="ml-3">
-                                    <h3 class="text-sm font-medium text-yellow-800 dark:text-yellow-200">
-                                        Action Required
-                                    </h3>
-                                    <div class="mt-2 text-sm text-yellow-700 dark:text-yellow-300">
-                                        <p>Your license expires soon. Set up automatic renewal now to avoid interruption.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-
                     <div class="bg-blue-50 dark:bg-blue-900/20 rounded-lg p-6 mb-6">
                         <h4 class="text-lg font-semibold text-blue-900 dark:text-blue-100 mb-2">
                             🎉 Early Access Pricing Available!
