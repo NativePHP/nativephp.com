58c2f1eca6e713db4740d48e31e4385c5a8f373e
Simon Hamp <simon.hamp@me.com>
2026-02-15 22:41:43 +0000

    The Vibes banner

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -116,7 +116,7 @@
         "
         class="font-poppins min-h-screen overflow-x-clip antialiased selection:bg-black selection:text-[#b4a9ff] dark:bg-[#050714] dark:text-white"
     >
-        <x-mobile-free-banner />
+        <x-the-vibes-banner />
 
         <x-navigation-bar />
 
